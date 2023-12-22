1d9cfeb48a1dac0230a159fa948eff414921a816
Prashant Dev <prashant.dixit@firminiq.com>
2023-12-22 14:56:35 +0530

    fix

--- createUser/constant.js
+++ createUser/constant.js
@@ -5,9 +5,33 @@ exports.VALIDATION_MESSAGES = {
 	INVALID_EMAIL: 'INVALID EMAIL'
 };
 exports.ERRORS = {
-	INVALID_EMAIL: {
+	USER_ID_REQUIRED: {
+		CODE: 'USER_ID_REQUIRED',
+		MESSAGE: 'User Id Required'
+	},
+    INVALID_EMAIL: {
 		CODE: 'INVALID_EMAIL',
-		MESSAGE: 'Please provid a valid email'
+		MESSAGE: 'Please provide a valid email'
+	},
+    INVALID_USER: {
+		CODE: 'INVALID_USER',
+		MESSAGE: 'Please provide user name'
+	},
+    INVALID_ADDRESS: {
+		CODE: 'INVALID_ADDRESS',
+		MESSAGE: 'Please provide user address'
+	},
+    INVALID_PROCESS_STATUS: {
+		CODE: 'INVALID_PROCESS_STATUS',
+		MESSAGE: 'Please provide process status'
+	},
+    INVALID_DOCTOR: {
+		CODE: 'INVALID_DOCTOR',
+		MESSAGE: 'Please provide assignedDoctor'
+	},
+    INVALID_HOSPITAL: {
+		CODE: 'INVALID_HOSPITAL',
+		MESSAGE: 'Please provide hospital ID'
 	}
 }
 exports.EMAIL_REGEX = new RegExp(/^[a-zA-Z0-9](?!.*?[^\na-zA-Z0-9]{2})[^\s@]+@[^\s@]+\.[^\s@]+[a-zA-Z0-9]$/);
