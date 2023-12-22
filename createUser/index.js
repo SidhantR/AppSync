76b9e04de07411e50628fa002a91abf9da53325d
Prashant Dev <prashant.dixit@firminiq.com>
2023-12-22 15:14:53 +0530

    fix

--- createUser/index.js
+++ createUser/index.js
@@ -5,8 +5,9 @@ const Validation = require('./validateInput');
 exports.handler = async (query) => {
     try {
         const obj = query.arguments.input;
+        obj.userId = Date.now().toString();
         Validation.validateData(obj);
-        
+
         await DB.saveUpdateItem(obj, CONSTANTS.USER_TABLE);
         return obj;
     } catch (error) {
